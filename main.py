437046157b8a49cf8a1b9d7fefffe89fe9351c9d
JAFF <63846848+JAFF-CYBERTHEIF@users.noreply.github.com>
2021-07-14 20:48:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 def move_zeros(array):
     i=0
     j=0
-
+#loop to detect and delete 0's
     while i< len(array):
         if array[i]==0:
             del array[i]
             j+=1
         else:
             i+=1
-
+# loop to append the deleted 0's at the end of the array
     for k in range(j):
         array.append(0)
            
